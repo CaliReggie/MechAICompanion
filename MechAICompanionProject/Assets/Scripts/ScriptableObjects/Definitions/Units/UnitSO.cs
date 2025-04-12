7be0b7e3600ee151a1862d57fda2f120e9f43aa7
Sid Greenwood <green272@msu.edu>
2025-04-12 17:02:26 -0400

    .

--- MechAICompanionProject/Assets/Scripts/ScriptableObjects/Definitions/Units/UnitSO.cs
+++ MechAICompanionProject/Assets/Scripts/ScriptableObjects/Definitions/Units/UnitSO.cs
@@ -2,18 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum eTeam
+{
+    AllRounder,
+    Defender,
+    Rushdown,
+    Ranger,
+    Alien
+}
+
+public enum eMechType
+{
+    Swordfighter,
+    Shield,
+    Medic,
+    Rifleman,
+    CombatKnife,
+    EnergySword,
+    Railgunner
+}
+
+public enum eAlienType
+{
+    Grunt,
+    Brute,
+    Tank,
+    Sniper,
+    Support
+}
+
 [CreateAssetMenu(fileName = "New Unit", menuName = "ScriptableObjects/UnitSO")]
 public class UnitSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Team Settings")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public eTeam team;
+
+    [Header("Mech Classification")]
+
+    public eMechType mechClass;
+
+    [Header("Alien Classification")]
+
+    public eAlienType alienClass;
+
 }
